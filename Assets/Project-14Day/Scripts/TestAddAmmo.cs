509d6af56e0d6e1351a715ca9b846cd8dfe7198e
ifairuss <ifairuss2403artem@gmail.com>
2025-11-20 14:46:10 +0100

    Feature/AddDebugItem(AmmoZone)

---
+++ Assets/Project-14Day/Scripts/TestAddAmmo.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TestAddAmmo : MonoBehaviour
+{
+    private float _timer;
+    private float _timeToAddAmmo = 3f;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            var player = collision.gameObject.GetComponentInChildren<Guns>();
+
+            if (_timer <= 0)
+            {
+                player.AddAmmo(1);
+                _timer = _timeToAddAmmo;
+            }
+            else
+            {
+                _timer -= 0.1f;
+            }
+        }
+    }
+}
